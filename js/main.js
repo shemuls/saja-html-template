552d5b0b9c5334bc22611d60218eefa63a04997b
shemuls <shemulpro@gmail.com>
2022-04-28 12:01:28 +0600

    Responsive update

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-var className = "bg-secondary";
-var scrollTrigger = 60;
-window.onscroll = function () {
-  // We add pageYOffset for compatibility with IE.
-  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
-    document.getElementsByTagName("header")[0].classList.add(className);
-  } else {
-    document.getElementsByTagName("header")[0].classList.remove(className);
-  }
-};
-
 jQuery(".mobileMenu-openIcon").click(function () {
   jQuery(".header-main-menu").toggleClass("show");
 });
